5e5bdb854545a71847e7a6554f57fa052a459e6e
caiocapua <caiocapua@hotmail.com>
2022-02-16 16:59:12 -0300

    m

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -100,7 +100,7 @@ export default function Home() {
       <Header />
       <main className={styles.contentContainer}>
         <section className={styles.contentIntroduction}>
-          <img className={styles.imageIllustration} src='images/illustration.svg' alt="Imagem illustrativa introdução" />
+          <img src='images/illustration.svg' className={styles.imageIllustration} alt="Imagem illustrativa introdução" />
           <div className={styles.content}>
             <h1>
               Ready for <br />
